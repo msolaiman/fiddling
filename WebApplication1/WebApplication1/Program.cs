9f45449b7648bea33b875bc75ac2bd783b29f02a
Moatasim <moatasims.it@gmail.com>
2020-10-09 18:08:18 +0300

    added a comment while in the main branch

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication1
         // program class
         public static void Main(string[] args)
         {
+            // comment in main branch
             CreateHostBuilder(args).Build().Run();
         }
 
